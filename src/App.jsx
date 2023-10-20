5f26bd9e25bf45f88798d6aa97daba050236726e
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2023-10-20 02:34:28 +0100

    Feat: implemeted routes using the BrowserRouter from react-router-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import NotFound from "./pages/404-NotFound"
 import NavBar from "./component/layouts/NavBar"
 import Footer from "./component/layouts/Footer"
 
@@ -10,8 +13,13 @@ const App = () => {
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
 
-        <main>
-          content
+        <main className="container mx-auto px3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
         </main>
 
         <Footer />
