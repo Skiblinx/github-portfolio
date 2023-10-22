e9bff11649ed2f83bfaa9d7cde3cae798107a079
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2023-10-22 21:21:39 +0100

    Fix: userItem path fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,38 @@ import NotFound from "./pages/404-NotFound"
 import NavBar from "./component/layouts/NavBar"
 import Footer from "./component/layouts/Footer"
 import Alert from "./component/layouts/Alert"
-
 import { GithubProvider } from "./context/github/GithubContext"
 import { AlertProvider } from "./context/alert/AlertContext"
 import User from "./pages/User"
-import ErrorBoundary from "./component/ErrorBoundary"
-// import ErrorTest from './pages/ErrorTest'
+
 
 const App = () => {
 
 
   return (
-    <ErrorBoundary>
-      <GithubProvider>
-        <AlertProvider>
-          <Router>
-            <div className="flex flex-col justify-between h-screen">
-              <NavBar />
 
-              <main className="container mx-auto px3 pb-12">
-                <Alert />
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/about" element={<About />} />
-                  {/*} <Route path="/errorTest" element={<ErrorTest />} />*/}
-                  <Route path="/user/:login" element={<User />} />
-                  <Route path="/notfound" element={<NotFound />} />
-                  <Route path="/*" element={<NotFound />} />
-                </Routes>
-              </main>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <NavBar />
 
-              <Footer />
-            </div>
-          </Router>
-        </AlertProvider>
-      </GithubProvider>
-    </ErrorBoundary>
+            <main className="container mx-auto px3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   )
 }
 
