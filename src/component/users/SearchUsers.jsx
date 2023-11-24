695c69e3f5a1bdb8d188ad8c7ffc114b56f737e5
Adeleye Abdulbasit Damilola <adeleyeabdulbasitdamilola@gmail.com>
2023-11-24 06:24:18 +0100

    fix: fixed an infinite loop

--- src/component/users/SearchUsers.jsx
+++ src/component/users/SearchUsers.jsx
@@ -31,7 +31,7 @@ const SearchUsers = () => {
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
-              <input className="w-full pr-40 bg-gray-200 input text-black " placeholder="Search..." value={text} onChange={handleChange} />
+              <input className="w-full pr-40 bg-gray-200 input text-black " placeholder="Skiblinx" value={text} onChange={handleChange} />
               <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn">Go</button>
             </div>
           </div>
